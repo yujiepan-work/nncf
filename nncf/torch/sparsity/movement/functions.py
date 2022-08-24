353935daa846b15b4e7546d371a2f9b8b2327501
yujiepan <yujie.pan@intel.com>
2022-08-25 01:06:12 +0800

    enable nosigmoid

--- nncf/torch/sparsity/movement/functions.py
+++ nncf/torch/sparsity/movement/functions.py
@@ -11,19 +11,54 @@
  limitations under the License.
 """
 
+import os
 import torch
+import math
 
 from nncf.torch.dynamic_graph.patch_pytorch import register_operator
 from nncf.torch.functions import STThreshold
 
 @register_operator()
-def binary_mask_by_threshold(importance, threshold=0.5, sigmoid=True, max_percentile=0.98):
-    with torch.no_grad():
-        if sigmoid is True:
+def binary_mask_by_threshold(importance, threshold=0.5, sigmoid=int(os.environ.get('YUJIE_SIGMOID_THRESHOLD', '1')), max_percentile=0.98):
+    if sigmoid:
+        with torch.no_grad():
             max_threshold = torch.quantile(torch.sigmoid(importance), q=max_percentile).item()
-        else:
-            max_threshold = torch.quantile(importance, q=max_percentile).item()
-    
-    if sigmoid is True:
         return STThreshold.apply(torch.sigmoid(importance), min(threshold, max_threshold))
-    return STThreshold.apply(importance, min(threshold, max_threshold))
+    else:
+        with torch.no_grad():
+            max_threshold = torch.quantile(importance, q=max_percentile).item()
+        return STThreshold.apply(importance, min(threshold, max_threshold))
+
+# @register_operator()
+# def binary_mask_by_threshold(importance, threshold=0.5, sigmoid=(os.environ.get('NNCF_THRESHOLD_SIGMOID', 'true').lower() == 'true'), max_percentile=0.98):
+#     if sigmoid:
+#         with torch.no_grad():
+#             max_threshold = torch.quantile(torch.sigmoid(importance), q=max_percentile).item()
+#         result_sig = STThreshold.apply(torch.sigmoid(importance), min(threshold, max_threshold))
+#         with torch.no_grad():
+#             max_threshold_nosig = torch.quantile(importance, q=max_percentile).item()
+#         threshold_nosig = -2.197224577336219
+#         result_nosig = STThreshold.apply(importance, min(threshold_nosig, max_threshold_nosig))
+#         err = (result_sig - result_nosig).square().sum()
+#         if err > 0:
+#             print('!!!', err, result_nosig, result_sig, flush=True)
+#         return result_nosig
+#     else:
+#         with torch.no_grad():
+#             max_threshold = torch.quantile(importance, q=max_percentile).item()
+#         return STThreshold.apply(importance, min(threshold, max_threshold))
+
+# @register_operator()
+# def binary_mask_by_threshold(importance, threshold=0.5, sigmoid=(os.environ.get('NNCF_THRESHOLD_SIGMOID', 'true').lower() == 'true'), max_percentile=0.98):
+#     if int(os.environ.get('YUJIE_SIGMOID_THRESHOLD', '1')):
+#         with torch.no_grad():
+#             max_threshold = torch.quantile(torch.sigmoid(importance), q=max_percentile).item()
+#         result_sig = STThreshold.apply(torch.sigmoid(importance), min(threshold, max_threshold))
+#         return result_sig
+#     else:
+#         with torch.no_grad():
+#             max_threshold_nosig = torch.quantile(importance, q=max_percentile).item()
+#         threshold_nosig = -math.inf
+#         if threshold > 0:
+#             threshold_nosig =  math.log(threshold/ (1-threshold))
+#         return STThreshold.apply(importance, min(threshold_nosig, max_threshold_nosig))
--- nncf/torch/sparsity/movement/layers.py
+++ nncf/torch/sparsity/movement/layers.py
@@ -11,6 +11,8 @@
  limitations under the License.
 """
 from typing import List
+import os
+import math
 
 import torch
 
@@ -59,7 +61,7 @@ class MovementSparsifier(nn.Module):
         self.frozen = frozen
         self.eps = eps
         self.lmbd = 0.5 # module_level_loss_weightage
-        self.masking_threshold = 0.0
+        self.masking_threshold = 0.0 if int(os.environ.get('YUJIE_SIGMOID_THRESHOLD', '1')) else -math.inf
         self.sparse_cfg = sparse_cfg
         
         weight_shape = target_module_node.layer_attributes.get_weight_shape()
